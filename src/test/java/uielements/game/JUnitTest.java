6a699bde582db0f7834d4920f4d7386a0dd4b508
Lucy <lucywoods184@gmail.com>
2022-12-22 18:20:50 +0000

    Adding getter and setter test

--- src/test/java/uielements/game/JUnitTest.java
+++ src/test/java/uielements/game/JUnitTest.java
@@ -2,10 +2,23 @@ package uielements.game;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.sql.Date;
+
 public class JUnitTest {
     @Test
     public void testConstructor() {
-
+    }
+    @Test
+    public void testGettersAndSetters(){
+        Actor actor1 = new Actor();
+        actor1.setActorid(5);
+        Assertions.assertEquals(5,actor1.getActorid(), "Actor id error");
+        actor1.setFirst_name("Firstnametest");
+        Assertions.assertEquals("Firstnametest",actor1.getFirst_name(), "First name error");
+        actor1.setLast_name("Lastnametest");
+        Assertions.assertEquals("Lastnametest",actor1.getLast_name(), "Last name error");
+        actor1.setBirthday(Date.valueOf("1900-01-01"));
+        Assertions.assertEquals(Date.valueOf("1900-01-01"),actor1.getBirthday(),"Birthday setter/getter error");
     }
 
 }
